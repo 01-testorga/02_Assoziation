0c7a7aa0162df28af96fbf9debb176f3317adfe3
Randy <Bilz.Randy@gmail.com>
2022-03-31 15:17:34 +0200

    keiinen plan

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,11 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+    //  System.out.println ("Hello wOrld");
+    //  System.out.println ("Pi:" +Math.PI);
+    
+    Helper.ausgabe("Hello wOrld");
+    Helper.ausgabe(2);
     }
 
 }
