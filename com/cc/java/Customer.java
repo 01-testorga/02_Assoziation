e53397dd45c2550c3a007ccec3510a31a96ebe6c
Randy <Bilz.Randy@gmail.com>
2022-04-01 10:47:32 +0200

    zwischenstand 4

--- com/cc/java/Customer.java
+++ com/cc/java/Customer.java
@@ -5,24 +5,27 @@ public class Customer {
     private String name; 
     private String city;
 
+    private Order order;
+
     public Customer(int id, String name, String city) {
         this.id = id;
         this.name = name;
         this.city = city;
 
-        Helper.ausgabe(this);
+
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-    public void setCity(String city) {
-        this.city = city;
-    }
-    public void setName(String name) {
-        this.name = name;
+    // assoziation
+    public void initOrder(){
+        order = new Order(1,"Repair");
     }
 
+    public void removeOrder(){
+        order = null;
+    }
+
+
+    //getter
     public String getName() {
         return name;
     }
@@ -33,5 +36,15 @@ public class Customer {
         return id;
     }
 
+    // setter
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
     
 }
