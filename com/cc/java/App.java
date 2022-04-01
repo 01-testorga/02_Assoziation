3f8e2b80f6cc43951f1dcc4c167bec09af1ff2e9
Randy <Bilz.Randy@gmail.com>
2022-04-01 11:37:32 +0200

    zwischenstand 7

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,10 +5,13 @@ public class App {
     public static void main(String[] args) {
         
         Customer customer = new Customer (1, "Max Muster", "beispielstadt");
+        
         customer.initOrder();
         Helper.ausgabe(customer.getOrder());
+        customer.checkOrder();
         customer.removeOrder();
         Helper.ausgabe(customer.getOrder());
+        customer.checkOrder();
     }
 
 }
